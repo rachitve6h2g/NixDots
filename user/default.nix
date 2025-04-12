dfc53ffc1ecd49bc3ef4d18729017db78df688c7
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-04-12 13:17:38 +0530

    Starship tinkered with

--- user/default.nix
+++ user/default.nix
@@ -23,5 +23,9 @@
     ./Hyprland
     ./Programs
   ];
+
+  # TODO: see how to make a custom module for this
+  # Instead of doing myTheme everywhere with a let binding
+  # Use it as a custom module
   colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
 }
--- user/sh.nix
+++ user/sh.nix
@@ -1,9 +1,11 @@
-{ lib, ... }:
+{ lib, config, ... }:
 let
   myAliases = {
     update = "sudo nixos-rebuild switch";
     cleanall = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
   };
+
+  myTheme = config.colorScheme.palette;
 in
 {
   # home = {
@@ -54,6 +56,54 @@ in
       enableZshIntegration = true;
 
       # TODO: Set a catppuccin starship configuration
+      settings = {
+        add_newline = true;
+
+        format = lib.concatStrings [
+          "[](color_orange)"
+          "$os"
+          "$username"
+          "[](bg:color_yellow fg:color_orange)"
+          "$directory"
+          "[](fg:color_yellow bg:color_aqua)"
+          "$git_branch"
+          "$git_status"
+          "[](fg:color_aqua bg:color_blue)"
+          "$c"
+          "$rust"
+          "$golang"
+          "$nodejs"
+          "$php"
+          "$java"
+          "$kotlin"
+          "$haskell"
+          "$python"
+          "[](fg:color_blue bg:color_bg3)"
+          "$docker_context"
+          "$conda"
+          "[](fg:color_bg3 bg:color_bg1)"
+          "$time"
+          "[ ](fg:color_bg1)"
+          "$line_break$character"
+        ];
+
+        palette = "\'catpuccin_mocha\'";
+
+        palettes.catppuccin_mocha = [
+          {
+            color_fg0 = "#${myTheme.base05}";
+            color_bg1 = "#${myTheme.base02}";
+            color_bg3 = "#${myTheme.base03}";
+            color_blue = "#${myTheme.base0D}";
+            color_aqua = "#${myTheme.base0C}";
+            color_green = "#${myTheme.base0C}";
+            color_orange = "#${myTheme.base09}";
+            color_purple = "#${myTheme.base0E}";
+            color_red = "#${myTheme.base08}";
+            color_yellow = "#${myTheme.base0A}";
+          }
+        ];
+      };
     };
 
     eza = {
