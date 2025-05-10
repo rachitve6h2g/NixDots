fbde00a77c1bdb86635357b97fb00e290abaea45
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-05-10 11:01:51 +0530

    Changed wlsunset user service

--- modules/user/Hyprland/wlsunset.nix
+++ modules/user/Hyprland/wlsunset.nix
@@ -1,13 +1,23 @@
-{ pkgs, ... }: {
-	services.wlsunset = {
-		enable = true;
-		package = pkgs.wlsunset;
-		
-		sunset = "18:00";
-		sunrise = "08:00";
-		temperature = {
-			day = "7000";
-			night = "5000";
-		};
-	};
+{ pkgs, lib, ... }: {
+  systemd.user.services = lib.mkForce {
+    wlsunset = {
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+
+      Unit = {
+        Description = "Day/night gamma adjustments for Wayland";
+        Documentation = ["man:wlsunset(1)"];
+        After = ["graphical-session.target"];
+      };
+
+      Service = {
+        Type = "exec";
+        ExecStart = "${pkgs.wlsunset}/bin/wlsunset -t 5000 -T 7000 -s 18:00 -S 06:30";
+        ExecCondition = "${pkgs.systemd}/lib/systemd/systemd-xdg-autostart-condition \"Hyprland\" \"\" ";
+        Restart = "on-failure";
+        Slice = "background-graphical.slice";
+      };
+    };
+  };
 }
