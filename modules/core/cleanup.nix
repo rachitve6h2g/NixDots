5f84dcb96acb083a64021717ba9b91ca61cfba93
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-07-16 00:15:39 +0530

    add: nh, remove: gc automatic

--- modules/core/cleanup.nix
+++ modules/core/cleanup.nix
@@ -1,9 +1,11 @@
+{ config, ... }:
 {
   nix = {
     settings.auto-optimise-store = true;
 
+    # When using programs.nh.clean.enable, don't use this.
     gc = {
-      automatic = true;
+      automatic = if config.programs.nh.clean.enable == true then false else true;
       dates = "daily";
       options = "--delete-older-than 3d";
     };
--- modules/core/nh.nix
+++ modules/core/nh.nix
@@ -1,11 +1,13 @@
 {
-	programs = {
-		 enable = true;
-		 clean = {
-		       enable = true;
-		       dates = "weekly";
-		       extraArgs = "--keep 5 --keep-since 3d";
-		 };
-		 flake = "/home/krish/.dotfiles";
-         };
+  programs = {
+    nh = {
+      enable = true;
+      clean = {
+        enable = true;
+        dates = "weekly";
+        extraArgs = "--keep 5 --keep-since 3d";
+      };
+      flake = "/home/krish/.dotfiles";
+    };
+  };
 }
