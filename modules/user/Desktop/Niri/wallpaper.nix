f4b6145c54817777dfae7fae850d6012d32cd0af
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-07-15 23:14:56 +0530

    change wallpaper, README: added note to self.

--- modules/user/Desktop/Niri/wallpaper.nix
+++ modules/user/Desktop/Niri/wallpaper.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, ... }: let 
-  wallpaper = "%h/.dotfiles/wallpapers/wallpaper.png";
+  wallpaper = "%h/.dotfiles/wallpapers/Manga-Portal-rosepine.png";
 in {
   # For making it conform with UWSM
   systemd.user.services = {
