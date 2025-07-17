60ad1f56241f50076d5e9400da61a00e7cb5a0ab
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-07-17 19:23:05 +0530

    add: host configuration for gh.

--- modules/user/Programs/git.nix
+++ modules/user/Programs/git.nix
@@ -57,6 +57,16 @@
       settings = {
         git_protocol = "ssh";
       };
+
+      hosts = {
+        "github.com" = {
+          git_protocol = "ssh";
+          users = {
+            rachitve6h2g = "";
+          };
+          user = "rachitve6h2g";
+        };
+      };
     };
 
     ssh = {
